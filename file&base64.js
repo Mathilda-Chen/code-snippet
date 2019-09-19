74c79d57238ba08b7bf477840159d7d6dc553006
Mathilda-Chen <36831056+Mathilda-Chen@users.noreply.github.com>
2019-09-19 10:29:09 +0800

    Update and rename imageUtil.js to file&base64.js

--- file&base64.js
+++ file&base64.js
@@ -1,4 +1,4 @@
-
+var code = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'.split('') //索引表
 /**
  * 文件转换base64
  */
@@ -68,6 +68,76 @@ function binToBase64(bitString) {
   return result;
 }
 
+
+/**
+ * base64转文件
+ */
+function base64ToFile(dataURI) {
+  // 写文件
+  return new Promise((resolve, reject) => {
+    var bin = base64ToBin(dataURI)
+    var Str = BinToStr(bin)
+    var buffer = stringToUint8Array(Str)
+    // var buffer = Base64Binary.decode(dataURI)
+    var newDateTime = (new Date()).valueOf()
+    var url = 'internal://files/work/' + newDateTime + 'pigmentation.jpg'
+    file.writeArrayBuffer({
+      uri: url,
+      buffer: buffer,
+      position: 0,
+      success: function () {
+        resolve(url)
+      },
+      fail: (data, code) => {
+        reject(data)
+      }
+    })
+  })
+}
+
+/**
+ * @description 将base64编码转换为二进制序列
+ * @param {String}
+ * @return {String}
+ */
+function base64ToBin(str) {
+  var bitString = "";
+  var tail = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] != "=") {
+      var decode = code.indexOf(str[i]).toString(2);
+      bitString += (new Array(7 - decode.length)).join("0") + decode;
+    } else {
+      tail++;
+    }
+  }
+  return bitString.substr(0, bitString.length - tail * 2);
+}
+
+/**
+ * @description 将二进制序列转换为字符串
+ * @param {String} Bin
+ */
+function BinToStr(Bin) {
+  var result = "";
+  for (var i = 0; i < Bin.length; i += 8) {
+    result += String.fromCharCode(parseInt(Bin.substr(i, 8), 2));
+  }
+  return result;
+}
+
+/**
+ * @description 将字符串换为Uint8Array
+ */
+function stringToUint8Array(str) {
+  var arr = [];
+  for (var i = 0, j = str.length; i < j; ++i) {
+    arr.push(str.charCodeAt(i));
+  }
+  var tmpUint8Array = new Uint8Array(arr);
+  return tmpUint8Array
+}
+
 export default = {
-  fileToBase64
+  fileToBase64, base64ToFile
 }
